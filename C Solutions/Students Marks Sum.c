3887d13c717af3922ab10e89943005a2887220b4
akhil1239 <78419720+akhil1239@users.noreply.github.com>
2021-02-02 18:34:28 +0530

    Create Students Marks Sum.c

---
+++ C Solutions/Students Marks Sum.c
@@ -0,0 +1,13 @@
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int x=0;
+  if(gender=='b'){
+      for(int i=0;i<number_of_students;i+=2) x+=marks[i];
+  }
+  if(gender=='g'){
+      for(int i=1;i<=number_of_students;i+=2) x+=marks[i];
+  }
+  return x;
+}
